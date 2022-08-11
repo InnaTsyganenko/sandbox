04d973ab9631726bfaabd972bd9d571dba38f546
Inna Tsyganenko <70580249+InnaTsyganenko@users.noreply.github.com>
2022-08-11 17:56:47 +0300

    edits

--- src/components/other/bem/bem.jsx
+++ src/components/other/bem/bem.jsx
@@ -6,6 +6,7 @@ function Bem() {
     <section className="conspect">
       <h1>Конспект по <abbr data-title="Блок, Элемент, Модификатор">БЭМ</abbr></h1>
       <p><a href="https://ru.bem.info/methodology/" target="_blank" rel="noopener noreferrer">МЕТОДОЛОГИЯ</a></p>
+      <p><a href="http://yoksel.github.io/easy-markup/bem-rules/" target="_blank" rel="noopener noreferrer">Как писать классы по БЭМ?</a></p>
       <p><a href="https://medium.com/@innabelaya/%D0%B1%D1%8D%D0%BC-%D0%B4%D0%BB%D1%8F-%D0%BD%D0%B0%D1%87%D0%B8%D0%BD%D0%B0%D1%8E%D1%89%D0%B8%D1%85-%D0%BE%D1%87%D0%B5%D0%B2%D0%B8%D0%B4%D0%BD%D1%8B%D0%B5-%D0%B8-%D0%BD%D0%B5%D0%BE%D1%87%D0%B5%D0%B2%D0%B8%D0%B4%D0%BD%D1%8B%D0%B5-%D0%B2%D0%BE%D0%BF%D1%80%D0%BE%D1%81%D1%8B-%D0%B2%D0%B5%D1%80%D1%81%D1%82%D0%BA%D0%B8-1a21d67cf840" target="_blank" rel="noopener noreferrer">БЭМ для начинающих. Очевидные и неочевидные вопросы верстки</a></p>
       <p><a href="https://habr.com/ru/company/yandex/blog/276035/" target="_blank" rel="noopener noreferrer">БЭМ-методология: с чего всё начиналось и зачем это всё нужно</a></p>
       <p><a href="https://css-live.ru/articles/bemantika-pishite-osmyslennye-stili-bez-povtorov.html" target="_blank" rel="noopener noreferrer">БЭМантика: пишите осмысленные стили без повторов</a></p>
@@ -42,7 +43,7 @@ function Bem() {
         <li>Каждая БЭМ-сущность должна иметь свой класс.</li>
         <li>CSS-свойства для блоков, элементов и модификаторов описываются только через классы.</li>
         <li>Для разделения слов в именах используется дефис (-).</li>
-        <li>Элемент отделяется от блока двумя подчеркиваниями (__). Модификатор — одним (_).</li>
+        <li>Элемент отделяется от блока двумя подчеркиваниями (__). Модификатор — двумя чёрточками(--).</li>
         <li>Имена БЭМ-сущностей записываются с помощью цифр и латинских букв в нижнем регистре.</li>
       </ol>
 
@@ -50,8 +51,8 @@ function Bem() {
       <ul>
         <li>Имя блока — <span className="hl-5">header</span>.</li>
         <li>Имя элемента блока — <span className="hl-5">header__search-form</span> — элемент search-form блока header.</li>
-        <li>Имя модификатора блока — <span className="hl-5">header_theme_green-forest</span> — модификатор theme в значении green-forest блока header.</li>
-        <li>Имя модификатора элемента — <span className="hl-5">header__search-form_disabled</span> — булев модификатор disabled элемента search-form блока header.</li>
+        <li>Имя модификатора блока — <span className="hl-5">header__theme--green-forest</span> — модификатор theme в значении green-forest блока header.</li>
+        <li>Имя модификатора элемента — <span className="hl-5">header__search-form--disabled</span> — булев модификатор disabled элемента search-form блока header.</li>
       </ul>
       <p>В простейшем случае блок соответствует DOM-узлу, один к одному. Но DOM-узел и блок — это не всегда одно и то же. На одном DOM-узле может совмещаться несколько сущностей. Это называется миксом. С помощью миксов можно:</p>
       <ul>
